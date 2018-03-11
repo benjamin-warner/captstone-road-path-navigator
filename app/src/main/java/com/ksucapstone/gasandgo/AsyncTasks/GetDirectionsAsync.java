86a08c8eb85fe334c12323d172245c79021592da
Ben Warner <benjaminnwarner@gmail.com>
2018-03-11 16:50:39 -0400

    Final route retreived

--- app/src/main/java/com/ksucapstone/gasandgo/AsyncTasks/GetDirectionsAsync.java
+++ app/src/main/java/com/ksucapstone/gasandgo/AsyncTasks/GetDirectionsAsync.java
@@ -1,8 +1,6 @@
 package com.ksucapstone.gasandgo.AsyncTasks;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ksucapstone.gasandgo.Models.Directions.DirectionsModel;
 
@@ -23,9 +21,15 @@ public class GetDirectionsAsync extends GetHttpAsync{
         String directionJson = jsonParser.parse(jsonResponse)
                 .getAsJsonObject().get("routes")
                 .getAsJsonArray().get(0)
-                .getAsJsonObject().get("legs")
-                .getAsJsonArray().get(0).toString();
+                .getAsJsonObject().toString();
         directions = new Gson().fromJson(directionJson, DirectionsModel.class);
+
+        String polyline = jsonParser.parse(jsonResponse)
+                .getAsJsonObject().get("routes")
+                .getAsJsonArray().get(0)
+                .getAsJsonObject().get("overview_polyline")
+                .getAsJsonObject().get("points").getAsString();
+        directions.polyline = polyline;
 
         callback.onResponseReceived(directions);
     }
--- app/src/main/java/com/ksucapstone/gasandgo/Models/Directions/DirectionsModel.java
+++ app/src/main/java/com/ksucapstone/gasandgo/Models/Directions/DirectionsModel.java
@@ -1,14 +1,8 @@
 package com.ksucapstone.gasandgo.Models.Directions;
 
-import com.ksucapstone.gasandgo.Models.GoogleApiLocation;
-
 import java.util.ArrayList;
 
 public class DirectionsModel {
-    public ArrayList<Step> steps;
-    public NumberWithLabel distance;
-    public NumberWithLabel duration;
-    public GoogleApiLocation start_location;
-    public GoogleApiLocation end_location;
-    public String overview_polyline;
+    public ArrayList<Leg> legs;
+    public String polyline;
 }
