417fd47a1175cdc2a256bb892bd7ea4d048574e1
Ben Warner <benjaminnwarner@gmail.com>
2018-03-11 12:32:04 -0400

    Tests passing for directioniterator

--- app/src/main/java/com/ksucapstone/gasandgo/Iterators/DirectionsIterator.java
+++ app/src/main/java/com/ksucapstone/gasandgo/Iterators/DirectionsIterator.java
@@ -1,6 +1,7 @@
 package com.ksucapstone.gasandgo.Iterators;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.ksucapstone.gasandgo.Helpers.DistanceHelper;
 import com.ksucapstone.gasandgo.Models.CarModel;
 import com.ksucapstone.gasandgo.Models.Directions.DirectionsModel;
 import com.ksucapstone.gasandgo.Models.Directions.Step;
@@ -20,7 +21,8 @@ public class DirectionsIterator {
 
         ArrayList<LatLng> refillPoints = new ArrayList<>();
         for(Step step : steps){
-            currentTakeCapacity = currentTakeCapacity - (step.distance.value / car.Mpg);
+            double stepDistanceInMiles = DistanceHelper.MetersToMiles(step.distance.value);
+            currentTakeCapacity = currentTakeCapacity - (stepDistanceInMiles / car.Mpg);
             if(currentTakeCapacity < (car.TankCapacity / 4)){
                 LatLng refillLocation = new LatLng(step.start_location.lat, step.start_location.lng);
                 refillPoints.add(refillLocation);
--- app/src/test/java/com/ksucapstone/gasandgo/Iterators/DirectionsIteratorShould.java
+++ app/src/test/java/com/ksucapstone/gasandgo/Iterators/DirectionsIteratorShould.java
@@ -9,12 +9,7 @@ import com.ksucapstone.gasandgo.TestHelpers.CarHelper;
 import com.ksucapstone.gasandgo.TestHelpers.StepHelper;
 
 import org.junit.Test;
-
-
-import org.junit.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.Assert.*;
 
 public class DirectionsIteratorShould {
@@ -22,41 +17,41 @@ public class DirectionsIteratorShould {
     @Test
     public void returnFourRefillLocationsForFourLongSteps() throws Exception {
         ArrayList<Step> steps = new ArrayList<>();
-        int fiveMilesInMeters = (int)DistanceHelper.MilesToMeters(5);
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(0,0)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(1,1)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(2,2)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(3,3)));
+        int fourMilesInMeters = (int)DistanceHelper.MilesToMeters(4);
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(0,0)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(1,1)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(2,2)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(3,3)));
 
         DirectionsModel directionsModel = new DirectionsModel();
         directionsModel.steps = steps;
 
-        CarModel car = CarHelper.MakeCar(5, 4);
+        CarModel car = CarHelper.MakeCar(1, 4);
 
         DirectionsIterator directionsIterator = new DirectionsIterator(directionsModel);
 
         ArrayList<LatLng> locations = directionsIterator.FindRefillPointsForCar(car);
-        assertEquals(locations.size(), 4);
+        assertEquals(4, locations.size());
     }
 
     @Test
     public void returnOneLocationForCarThatCanTotallyDoThat() throws Exception {
         ArrayList<Step> steps = new ArrayList<>();
-        int fiveMilesInMeters = (int)DistanceHelper.MilesToMeters(5);
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(0,0)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(1,1)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(2,2)));
-        steps.add(StepHelper.MakeStep(fiveMilesInMeters, new LatLng(3,3)));
+        int fourMilesInMeters = (int)DistanceHelper.MilesToMeters(4);
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(0,0)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(1,1)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(2,2)));
+        steps.add(StepHelper.MakeStep(fourMilesInMeters, new LatLng(3,3)));
 
         DirectionsModel directionsModel = new DirectionsModel();
         directionsModel.steps = steps;
 
-        CarModel car = CarHelper.MakeCar(17, 4);
+        CarModel car = CarHelper.MakeCar(1, 16);
 
         DirectionsIterator directionsIterator = new DirectionsIterator(directionsModel);
 
         ArrayList<LatLng> locations = directionsIterator.FindRefillPointsForCar(car);
-        assertEquals(locations.size(), 4);
+        assertEquals(1, locations.size());
     }
 
     @Test
@@ -76,6 +71,6 @@ public class DirectionsIteratorShould {
         DirectionsIterator directionsIterator = new DirectionsIterator(directionsModel);
 
         ArrayList<LatLng> locations = directionsIterator.FindRefillPointsForCar(car);
-        assertEquals(locations.size(), 0);
+        assertEquals(0, locations.size());
     }
 }
