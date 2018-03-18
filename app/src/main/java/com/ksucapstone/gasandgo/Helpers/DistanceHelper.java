e0a4376279dc4a29fa3b15d1054058eca3f56625
Ben Warner <benjaminnwarner@gmail.com>
2018-03-18 19:54:53 -0400

    Distance helpers added

--- app/src/main/java/com/ksucapstone/gasandgo/Helpers/DistanceHelper.java
+++ app/src/main/java/com/ksucapstone/gasandgo/Helpers/DistanceHelper.java
@@ -1,5 +1,9 @@
 package com.ksucapstone.gasandgo.Helpers;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+
 public class DistanceHelper {
 
     private static final double MilesInMeter = 0.000621371;
@@ -11,5 +15,26 @@ public class DistanceHelper {
 
     public static double MilesToMeters(double miles){
         return miles * MetersInMile;
+    }
+
+    public static double MilesBetween(LatLng a, LatLng b){
+        double distanceInMeters = GetDistance(a.latitude, a.longitude, b.latitude, b.longitude);
+        return MetersToMiles(distanceInMeters);
+    }
+
+    private static double GetDistance(double lat_a, double lng_a, double lat_b, double lng_b )
+    {
+        double earthRadius = 3958.75;
+        double latDiff = Math.toRadians(lat_b-lat_a);
+        double lngDiff = Math.toRadians(lng_b-lng_a);
+        double a = Math.sin(latDiff /2) * Math.sin(latDiff /2) +
+                Math.cos(Math.toRadians(lat_a)) * Math.cos(Math.toRadians(lat_b)) *
+                        Math.sin(lngDiff /2) * Math.sin(lngDiff /2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double distance = earthRadius * c;
+
+        int meterConversion = 1609;
+
+        return distance * meterConversion;
     }
 }
