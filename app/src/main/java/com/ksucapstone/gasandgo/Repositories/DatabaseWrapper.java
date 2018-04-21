4a371d8a95d218a877c05a1bf1a9b4ba6374652a
Ben Warner <benjaminnwarner@gmail.com>
2018-04-21 13:34:41 -0400

    Query methods for DB helper

--- app/src/main/java/com/ksucapstone/gasandgo/Repositories/DatabaseWrapper.java
+++ app/src/main/java/com/ksucapstone/gasandgo/Repositories/DatabaseWrapper.java
@@ -5,6 +5,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class DatabaseWrapper implements ValueEventListener {
@@ -25,6 +26,16 @@ public class DatabaseWrapper implements ValueEventListener {
     @Override
     public void onCancelled(DatabaseError databaseError) {
         //Todo: handle onCancelled errors
+    }
+
+    public void queryFor(String directory, String where, int equals){
+        Query query = mDatabaseReference.child(directory).orderByChild(where).equalTo(equals);
+        query.addListenerForSingleValueEvent(this);
+    }
+
+    public void queryFor(String directory, String where, String equals){
+        Query query = mDatabaseReference.child(directory).orderByChild(where).equalTo(equals);
+        query.addListenerForSingleValueEvent(this);
     }
 
     public void queryOnceForSingleObject(String directory) {
